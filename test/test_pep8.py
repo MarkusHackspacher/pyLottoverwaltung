66b52fccfcf0c4adbd1474b03d826490d4dc4948
MarkusHackspacher <hackspacher@gmx.de>
2015-10-13 18:18:22 +0200

    pep8 test

--- test/test_pep8.py
+++ test/test_pep8.py
@@ -1,31 +1,26 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-# pyLottoSimu
+# pyLottoverwaltung
 
 # Copyright (C) <2015> Markus Hackspacher
 
-# This file is part of pyLottoSimu.
+# This file is part of pyLottoverwaltung.
 
-# pyLottoSimu is free software: you can redistribute it and/or modify
+# pyLottoverwaltung is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# pyLottoSimu is distributed in the hope that it will be useful,
+# pyLottoverwaltung is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with pyLottoSimu.  If not, see <http://www.gnu.org/licenses/>.
-
+# along with pyLottoverwaltung.  If not, see <http://www.gnu.org/licenses/>.
 
 import unittest
 import pep8
-
-__author__ = 'mar'
 
 
 class TestCodeFormat(unittest.TestCase):
@@ -33,10 +28,12 @@ class TestCodeFormat(unittest.TestCase):
     def test_pep8_conformance(self):
         """Test that we conform to PEP8."""
         pep8style = pep8.StyleGuide(quiet=False)
-        result = pep8style.check_files(['lotto_gui_dateneing.py',
-                                        'datahandler.py',
-                                        'auswertung.py',
-                                        'kalender_datum.py'])
+        result = pep8style.check_files(['pylotto.pyw',
+                                        'test/test_pep8.py',
+                                        'lotto/auswertung.py',
+                                        'lotto/datahandler.py',
+                                        'lotto/kalender_datum.py',
+                                        'lotto/lotto_gui_dateneing.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
